24a5f089b90633361e0f0f2e8bbe7ab500e0c405
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-05-09 12:09:01 +0900

    chore: init project base

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+
+/** @type import('eslint').Linter.BaseConfig */
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error'
+  }
+};
